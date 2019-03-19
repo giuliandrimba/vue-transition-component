29d4cb5f6196519a7b38ee3d5dc92f28398f87ad
Giulian Drimba <giulian.drimba@mediamonks.com>
2019-03-19 16:36:49 +0100

    Update spec

--- test/util/PageComponentA/PageComponentA.ts
+++ test/util/PageComponentA/PageComponentA.ts
@@ -1,13 +1,14 @@
-import { Component } from 'vue-property-decorator';
-import { AbstractPageTransitionComponent } from '../../../src/lib/mixin/AbstractPageTransitionComponent';
+import AbstractPageTransitionComponent from '../../../src/lib/mixin/AbstractPageTransitionComponent';
 import PageComponentATransitionController from './PageComponentATransitionController';
 
-@Component({
-  template: `<div>PageAComponentA</div>`
-})
-export default class PageComponentA extends AbstractPageTransitionComponent {
-  handleAllComponentsReady() {
-    this.transitionController = new PageComponentATransitionController(this);
-    this.isReady();
-  }
-}
+export default {
+  name: 'PageComponentA',
+  extends: AbstractPageTransitionComponent,
+  template: `<div>PageAComponentA</div>`,
+  methods: {
+    handleAllComponentsReady() {
+      this.transitionController = new PageComponentATransitionController(this);
+      this.isReady();
+    },
+  },
+};
