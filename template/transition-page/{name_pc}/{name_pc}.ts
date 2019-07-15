b08c3aa2c308579baf9a5cfdf98bccad4aa7faaf
Giulian Drimba <giulian.drimba@mediamonks.com>
2019-07-15 13:48:29 +0200

    Update template

--- template/transition-page/{name_pc}/{name_pc}.ts
+++ template/transition-page/{name_pc}/{name_pc}.ts
@@ -3,8 +3,10 @@ import {{name_pc}}TransitionController from './{{name_pc}}TransitionController';
 
 @Component
 export default class {{name_pc}} extends AbstractPageTransitionComponent {
+  public transitionController: any;
+
   public handleAllComponentsReady(): void {
-    this.transitionController = new {{name_pc}}TransitionController(this);
+    this.transitionController = new {{name_pc}}TransitionController();
     this.isReady();
   }
 }
